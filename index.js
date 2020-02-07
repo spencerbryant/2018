96cdc8ff0b6f9ec3e4b576c09ca4496259e2c878
Spencer Bryant <spencerbryant@seattleacademy.org>
2020-02-07 09:10:22 -0800

    Add custom port number

--- index.js
+++ index.js
@@ -50,7 +50,7 @@ app.use('', express.static('public', { 'index': false }), serveIndex('public', {
 
 let server = http.createServer(app);
 
-const serverPort = 1500;
+const serverPort = 1507; 
 server.listen(serverPort);
 console.log('listening on port', serverPort)
 
